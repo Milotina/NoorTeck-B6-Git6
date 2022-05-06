9fa378751477dd22a6821a7df4fcbfcad836d604
milot <milot@DESKTOP-1PJF4EO>
2022-05-06 16:37:04 -0400

    changes...

--- src/test/java/git6/DemoSix.java
+++ src/test/java/git6/DemoSix.java
@@ -1,9 +1,13 @@
 package git6;
 
 public class DemoSix {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Demo Six...");
+	}
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
 	}
 
 }
